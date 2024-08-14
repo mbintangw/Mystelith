8d8a5ddf3bb0ee92141525e17b7d17c0b01589aa
mbintangw <mbintangw@gmail.com>
2024-08-14 12:40:08 +0700

    style : responsive

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const About = () => {
   return (
     <main className="flex justify-center items-center" id="About">
-      <div className="flex justify-center items-center gap-20 container">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-20 container">
         <img
           src="/Images/art.png"
           alt="artist"
-          className="w-[600px] h-[600px] object-cover"
+          className="lg:w-[600px] lg:h-[600px] w-[300px] h-[300px] object-cover"
         />
-        <div className="space-y-10 w-[500px]">
+        <div className="flex flex-col gap-10 justify-center items-center lg:justify-start lg:items-start">
           <h1 className="text-5xl font-bold">Art</h1>
-          <p className="text-justify">
+          <p className="text-justify w-2/3">
             Art is the expression of ideas and emotions through a physical
             medium, like painting, sculpture, film, dance, writing, photography,
             or theatre. If you love the creative process, maybe you'll devote
